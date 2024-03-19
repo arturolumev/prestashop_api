111d662688829fa98be4516d2a90f9bbce161b83
Arturo Lume <edwin.lume@tecsup.edu.pe>
2024-03-19 18:16:22 -0500

    cambios api

--- conexion.js
+++ conexion.js
@@ -1,65 +1,137 @@
-const http = require('http');
-const sql = require('mssql');
+const http = require("http");
+const sql = require("mssql");
 
 // Configuración de la conexión a la base de datos
 const config = {
-  user: 'kuky',
-  password: 'Kf123456',
-  server: '3.144.237.208',
-  database: 'prueba_kflor',
+  user: "kuky",
+  password: "Kf123456",
+  server: "3.144.237.208",
+  database: "prueba_kflor",
   options: {
-    encrypt: false // Si estás utilizando Azure, establece esto en true
-  }
+    encrypt: false, // Si estás utilizando Azure, establece esto en true
+  },
 };
 // Función para obtener los datos de la tabla y convertirlos en JSON
 async function obtenerDatosComoJSON() {
-    try {
-      // Conectar a la base de datos
-      await sql.connect(config);
-  
-      // Consulta para seleccionar los datos de la tabla
-      const result = await sql.query('SELECT * FROM personeria');
-  
-      // Convertir el resultado en formato JSON
-      const jsonData = result.recordset;
-  
-      // Cerrar la conexión
-      await sql.close();
-  
-      return jsonData;
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
+  try {
+    // Conectar a la base de datos
+    await sql.connect(config);
+
+    // Consulta para seleccionar los datos de la tabla
+    const result = await sql.query("SELECT * FROM personeria");
+
+    // Convertir el resultado en formato JSON
+    const jsonData = result.recordset;
+
+    // Cerrar la conexión
+    await sql.close();
+
+    return jsonData;
+  } catch (error) {
+    console.log(error);
+    throw error;
   }
-  
-  // Crear el servidor HTTP
-  const server = http.createServer(async (req, res) => {
-    // Verificar si la solicitud es para obtener los datos
-    if (req.url === '/datos') {
-      try {
-        // Obtener los datos de la base de datos como JSON
-        const jsonData = await obtenerDatosComoJSON();
-  
-        // Configurar la cabecera de respuesta para indicar que se está devolviendo JSON
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-  
-        // Enviar los datos como respuesta al navegador
-        res.end(JSON.stringify(jsonData));
-      } catch (error) {
-        // Enviar un mensaje de error si ocurrió un problema al obtener los datos
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Error al obtener los datos de la base de datos');
-      }
-    } else {
-      // Si la solicitud no es para obtener datos, devolver un mensaje de error
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end('Ruta no encontrada');
+}
+
+async function obtenerProdComoJSON() {
+  try {
+    // Conectar a la base de datos
+    await sql.connect(config);
+
+    // Consulta para seleccionar los datos de la tabla
+    const result = await sql.query("SELECT * FROM producto");
+
+    // Convertir el resultado en formato JSON
+    const jsonData = result.recordset;
+
+    // Cerrar la conexión
+    await sql.close();
+
+    return jsonData;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+async function obtenerPedidoCabeceraComoJSON() {
+  try {
+    // Conectar a la base de datos
+    await sql.connect(config);
+
+    // Consulta para seleccionar los datos de la tabla
+    const result = await sql.query("SELECT * FROM VentaPedidoCabecera");
+
+    // Convertir el resultado en formato JSON
+    const jsonData = result.recordset;
+
+    // Cerrar la conexión
+    await sql.close();
+
+    return jsonData;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+// Crear el servidor HTTP
+const server = http.createServer(async (req, res) => {
+  // Verificar si la solicitud es para obtener los datos
+  if (req.url === "/personeria") {
+    try {
+      // Obtener los datos de la base de datos como JSON
+      const jsonData = await obtenerDatosComoJSON();
+
+      // Configurar la cabecera de respuesta para indicar que se está devolviendo JSON
+      res.writeHead(200, { "Content-Type": "application/json" });
+
+      // Enviar los datos como respuesta al navegador
+      res.end(JSON.stringify(jsonData));
+    } catch (error) {
+      // Enviar un mensaje de error si ocurrió un problema al obtener los datos
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Error al obtener los datos de la base de datos");
     }
-  });
-  
-  // Escuchar en el puerto 3000
-  const PORT = 3000;
-  server.listen(PORT, () => {
-    console.log(`Servidor en ejecución en http://localhost:${PORT}`);
-  });
+  } else if (req.url === "/productos") {
+    try {
+      // Obtener los datos de la base de datos como JSON
+      const jsonData = await obtenerProdComoJSON();
+
+      // Configurar la cabecera de respuesta para indicar que se está devolviendo JSON
+      res.writeHead(200, { "Content-Type": "application/json" });
+
+      // Enviar los datos como respuesta al navegador
+      res.end(JSON.stringify(jsonData));
+    } catch (error) {
+      // Enviar un mensaje de error si ocurrió un problema al obtener los datos
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Error al obtener los datos de la base de datos");
+    }
+  } else if (req.url === "/pedidocabecera") {
+    try {
+      // Obtener los datos de la base de datos como JSON
+      const jsonData = await obtenerPedidoCabeceraComoJSON();
+
+      // Configurar la cabecera de respuesta para indicar que se está devolviendo JSON
+      res.writeHead(200, { "Content-Type": "application/json" });
+
+      // Enviar los datos como respuesta al navegador
+      res.end(JSON.stringify(jsonData));
+    } catch (error) {
+      // Enviar un mensaje de error si ocurrió un problema al obtener los datos
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Error al obtener los datos de la base de datos");
+    }
+  } else {
+    // Si la solicitud no es para obtener datos, devolver un mensaje de error
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Ruta no encontrada");
+  }
+});
+
+// Escuchar en el puerto 3000
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Servidor en ejecución en http://localhost:${PORT}`);
+});
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const axios = require("axios");
+const xml2js = require("xml2js");
+const parser = new xml2js.Parser({ explicitArray: false });
 
 const app = express();
 const port = 3099;
@@ -14,7 +16,7 @@ app.get("/api/products", async (req, res) => {
         params: {
           display: "full",
           output_format: "JSON",
-          limit: 3,
+          limit: 10,
         },
         headers: {
           Authorization: "ZBR3Q8MEZ3KC16C7Z5CMYYD9V1VFCT3T",
@@ -25,21 +27,36 @@ app.get("/api/products", async (req, res) => {
     // Extraer la información específica de cada producto
     const productsInfo = response.data.products.map((product) => {
       return {
-        name: product.name[0].value, // Si el nombre está disponible en el idioma predeterminado
-        reference: product.reference,
-        price: product.price,
+        //EmpresaId: product.name[0].value,
+        //ProductoId: product.reference,
+        Descripcion: product.meta_title[0].value,
+        Abreviatura: product.reference,
+        //GrupoId: "no tiene una categoria especificada", 
+        
+        // id: 050101 es PRESTASHOP 
+        // guardar por nombre no por id
+        
+        //UMUnitarioId: "no tiene",
+        StockMaximo: 0,
+        StocMinimo: 0,
+        MarcaId: 124,
+        Modelo: "",
+        Peso: 0,
+        Ubicacion: "",
+        Area: 0,
+        CodigoBarra: "",
+        CodigoAlterno: product.reference,
+        Estado: "1",
+        UsuarioID: 1,
+        FechaCreacion: product.date_add,
+        FechaModificacion: product.date_upd,
+        Idodoo: null,
+        Precio: 0,
+        Habilitado: 1,
       };
     });
 
-    // Mostrar la información por consola
-    productsInfo.forEach((product) => {
-      console.log(
-        `Nombre: ${product.name}, Referencia: ${product.reference}, Precio: ${product.price}`
-      );
-      console.log("---");
-    });
-
-    res.json(response.data);
+    res.json(productsInfo);
   } catch (error) {
     console.error(error);
     res.status(500).json({
@@ -68,26 +85,26 @@ app.get("/api/customers", async (req, res) => {
     // Extraer la información específica de cada cliente
     const customersInfo = response.data.customers.map((customer) => {
       return {
-        TipoIdentidadId: "buscar en otra tabla",
-        NroIdentidad: "Sacar de la tabla address",
+        //TipoIdentidadId: "buscar en otra tabla",
+        //NroIdentidad: "Sacar de la tabla address",
         GrupoPersoneria: 1,
-        Personeria: `${customer.name ? customer.name : ""}`, 
-        NombreComercial: `${customer.name ? customer.name : ""}`, 
-        Domiciliado: 1, 
-        TipoContribuyente: "1", 
-        FamiliaID: 143, 
-        NegocioID: 179, 
-        CtaDetraccion: "", 
-        Codigo: "", 
-        Estado: 1, 
-        UsuarioID: "1", 
-        FechaCreacion: `${customer.date_add ? customer.date_add : ""}`, 
-        FechaModificacion: `${customer.date_upd ? customer.date_add : ""}`, 
-        ConvenioID: 902.00001, 
-        MedioRegistroID: 900.00001, 
-        MedioInformacionID: 901.00001, 
-        Referencia: "", 
-        Telefonos: "sacar de la tabla address", 
+        Personeria: `${customer.name ? customer.name : ""}`,
+        NombreComercial: `${customer.name ? customer.name : ""}`,
+        Domiciliado: 1,
+        TipoContribuyente: "1",
+        FamiliaID: 143,
+        NegocioID: 179,
+        CtaDetraccion: "",
+        Codigo: "",
+        Estado: 1,
+        UsuarioID: "1",
+        FechaCreacion: `${customer.date_add ? customer.date_add : ""}`,
+        FechaModificacion: `${customer.date_upd ? customer.date_add : ""}`,
+        ConvenioID: 902.00001,
+        MedioRegistroID: 900.00001,
+        MedioInformacionID: 901.00001,
+        Referencia: "",
+        //Telefonos: "sacar de la tabla address",
         email: `${customer.email ? customer.email : ""}`,
       };
     });
@@ -122,7 +139,7 @@ app.get("/api/customers", async (req, res) => {
       console.log("---");
     });
 
-    res.json(customersInfo);
+    res.json(response.data);
   } catch (error) {
     console.error(error);
     res
@@ -131,20 +148,19 @@ app.get("/api/customers", async (req, res) => {
   }
 });
 
-// Endpoint para obtener las órdenes desde PrestaShop
-
-
-// Endpoint para obtener las órdenes desde PrestaShop
 app.get("/api/orders", async (req, res) => {
   try {
+    const page = req.query.page || 1; // Obtener el número de página de la consulta
+    const pageSize = req.query.pageSize || 10; // Obtener el tamaño de la página de la consulta
+
     const response = await axios.get(
       "https://ZBR3Q8MEZ3KC16C7Z5CMYYD9V1VFCT3T@www.kukyflor.com/api/orders",
       {
         params: {
-          display:
-            "all",
+          display: "full",
           output_format: "XML",
-          limit: 5,
+          page: page,
+          limit: pageSize,
         },
         headers: {
           Authorization: "ZBR3Q8MEZ3KC16C7Z5CMYYD9V1VFCT3T",
@@ -152,12 +168,102 @@ app.get("/api/orders", async (req, res) => {
       }
     );
 
-    res.json(response.data);
+    // Parsear la respuesta XML
+    parser.parseString(response.data, (err, result) => {
+      if (err) {
+        console.error(err);
+        return res
+          .status(500)
+          .json({ error: "Error al parsear la respuesta XML" });
+      }
+
+      // Extraer los elementos <order>
+      const orders = result.prestashop.orders.order;
+
+      // Extraer la información específica de cada cliente
+      const ordersInfo = result.prestashop.orders.order.map((order) => {
+        return {
+          PersoneriaID: order.id_customer,
+          OficinaAlmacenID: 1,
+          DireccionID: 1, //en la db hay 1 y 2
+          MonedaID: "102.00001",
+          ListaPrecioID: "108.00001",
+          estado: order.current_state,
+          ContadoEstado: 0,
+          VendedorId: "1",
+          TipoEntrega: 2,
+          FechaEntrega: order.delivery_date,
+          DireccionEntrega: order.id_address_delivery,
+          OficinaAlmacenEntregaID: "1",
+          referencia: order.reference,
+          Observaciones: "",
+          cliente: "",
+          Contacto: "",
+          ContactoTelefono: "",
+          tipoventa: 0,
+          HabilitarFecha: "0",
+          MotivoID: "190.00062", //por defecto
+          CondicionVtaID: "113.00001", //por defecto
+          DeliveryTipoID: "193.00001", //por defecto
+          DeliveryTurnoID: "192.00002", //por defecto
+          TipoDocID: "", // se jalara por address
+          ValorPedido: order.total_paid,
+          PrecioPedido: order.total_paid,
+          FacturarSwitch: 0,
+          ConvenioID: "902.00002", //por defecto
+          firstname: order.firstname,
+          lastname: order.lastname,
+          addres1: order.addres1,
+          addres2: order.addres2,
+          id_state: order.id_state,
+          phone: order.phone,
+          dniORUC: order.company,
+        };
+      });
+
+      res.json(orders);
+    });
   } catch (error) {
     console.error(error);
     res
       .status(500)
       .json({ error: "Error al obtener los pedidos de PrestaShop" });
+  }
+});
+
+app.get("/api/orders/:id", async (req, res) => {
+  const orderId = req.params.id;
+
+  try {
+    const response = await axios.get(
+      "https://ZBR3Q8MEZ3KC16C7Z5CMYYD9V1VFCT3T@www.kukyflor.com/api/orders/${orderId}",
+      {
+        params: {
+          output_format: "XML",
+        },
+        headers: {
+          Authorization: "ZBR3Q8MEZ3KC16C7Z5CMYYD9V1VFCT3T",
+        },
+      }
+    );
+
+    // Parsear la respuesta XML
+    parser.parseString(response.data, (err, result) => {
+      if (err) {
+        console.error(err);
+        return res
+          .status(500)
+          .json({ error: "Error al parsear la respuesta XML" });
+      }
+
+      // Extraer la orden con el ID especificado
+      const order = result.prestashop.order;
+
+      res.json(order);
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error al obtener la orden de PrestaShop" });
   }
 });
 
